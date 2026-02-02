bd895d159caee1c75bd128bfc194c595d541a7bb
wachipato <116815367+WACHIDADUCK@users.noreply.github.com>
2026-02-02 12:59:26 +0000

    fix: test/index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,16 +1,21 @@
-import app from "../src/app";
-import request from "supertest";
+import app from '../src/app';
+import request from 'supertest';
+import { pool } from '../src/db';
 
-describe("Index Routes", () => {
-  it("should respond welcome", async () => {
-    const res = await request(app).get("/");
+describe('Index Routes', () => {
+  it('should respond welcome', async () => {
+    const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toEqual({ message: "welcome to my api" });
+    expect(res.body).toEqual({ message: 'welcome to my api' });
   });
 
-  it("should respond pong", async () => {
-    const res = await request(app).get("/ping");
+  it('should respond pong', async () => {
+    const res = await request(app).get('/ping');
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toEqual({ result: "pong" });
+    expect(res.body).toEqual({ result: 'pong' });
+  });
+
+  afterAll(async () => {
+    await pool.end();
   });
 });
